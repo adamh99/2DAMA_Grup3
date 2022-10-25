812b7555d09291919f426e2e48c6f9cfbf1dea42
adamh99 <a21adahidsan@inspedralbes.cat>
2022-10-25 13:18:40 +0200

    APIConnection class

---
+++ app/src/main/java/com/example/myapplication/APIConnection.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import android.os.AsyncTask;
+
+public class APIConnection extends AsyncTask<String, Void, String> {
+
+    String api;
+
+    public APIConnection(String api_url){
+        api = api_url;
+    }
+    @Override
+    protected String doInBackground(String... strings) {
+
+        return null;
+    }
+
+    public boolean auth(String mail, String pass){
+
+        return false;
+    }
+}
