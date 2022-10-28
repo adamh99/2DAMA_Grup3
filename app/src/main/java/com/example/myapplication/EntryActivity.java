a5d5a93ee7c1b8cae4eda1728ca354aafeaaa23d
adamh99 <a21adahidsan@inspedralbes.cat>
2022-10-28 13:27:57 +0200

    fix

--- app/src/main/java/com/example/myapplication/EntryActivity.java
+++ app/src/main/java/com/example/myapplication/EntryActivity.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.prefs.Preferences;
+
 //esta actividad no tiene layout, solo sirve para ver si el usuario es nuevo o no y enseñarle la bienvenida o directamente el login
 public class EntryActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
@@ -28,10 +31,13 @@ public class EntryActivity extends AppCompatActivity {
     public boolean firstTime(){
         boolean firstTime = true;
         sharedPreferences = getPreferences(MODE_PRIVATE);
+
         if(sharedPreferences.contains("welcomed")){
             firstTime = false;
         }else {
-            sharedPreferences.edit().putBoolean("welcomed", true);
+            SharedPreferences.Editor edit = sharedPreferences.edit();
+            edit.putBoolean("welcomed", true);
+            edit.commit();
         }
         return firstTime;
     }
