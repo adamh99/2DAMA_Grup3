b320d91e4d55d775080408bbf9341c86ee5ffaba
marsanbla <a21marsanbla@inspedralbes.cat>
2022-11-08 09:46:45 +0100

    Dessign test

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,13 +2,14 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+private static final String LOG_TAG= LoginActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +23,13 @@ public class LoginActivity extends AppCompatActivity {
         String credentials = usernameField.getText().toString() + " " + usernameField.getText().toString();
 
         new Auth().execute(credentials);
+
+    }
+
+    public void recyclertest(View view) {
+        Log.d(LOG_TAG, "TEST RECYCLER SUCCES");
+        Intent intent = new Intent(this,recyclerview.class);
+        startActivity(intent);
 
     }
 }
