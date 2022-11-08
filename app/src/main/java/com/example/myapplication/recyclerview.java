f1fc1af672c0ca056161b9f25ea4fc4791a77a7b
adam <a21adahidsan@gmail.com>
2022-11-08 13:16:39 +0100

    Recyclerview medio hecha

--- app/src/main/java/com/example/myapplication/recyclerview.java
+++ app/src/main/java/com/example/myapplication/recyclerview.java
@@ -1,14 +1,27 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class recyclerview extends AppCompatActivity {
+import java.util.LinkedList;
 
+public class recyclerview extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private  WordListAdapter mAdapter;
+private final LinkedList<String> mWordList = new LinkedList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recyclerview);
+        for (int i = 0; i<20; i++){
+            mWordList.addLast("Word " +i);
+        }
+        mRecyclerView= findViewById(R.id.recyclerview);
+        mAdapter = new WordListAdapter(this, mWordList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
