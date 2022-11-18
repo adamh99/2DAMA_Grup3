aa0e185a0a18a8bc8c080f12f24e79fabb32b213
marsanbla <a21marsanbla@inspedralbes.cat>
2022-11-18 12:17:29 +0100

    Commit

--- Front-end/js/menu.js
+++ Front-end/js/menu.js
@@ -1,21 +1,18 @@
 new Vue({
-    el: '#menu',
-    vuetify: new Vuetify(),
-    data: () => ({
-      currentItem: 'tab-Web',
-      items: [
-     
-        {
-          name : 'Rewards',
-          
-        },
-      
-      ],
-    
-      selectIndexUser: 0,
-      itemsUser: [
-          { id: 'param', text: 'Paramètres', icon: 'mdi-cog-outline' },
-          { id: 'logout', text: 'LOG-OUT', icon: 'mdi-power' },
-      ],
-    })
-  })
+  el: "#menu",
+  vuetify: new Vuetify(),
+  data: () => ({
+    currentItem: "tab-Web",
+    items: [
+      {
+        name: "Panel",
+      },
+    ],
+
+    selectIndexUser: 0,
+    itemsUser: [
+      { id: "param", text: "Paramètres", icon: "mdi-cog-outline" },
+      { id: "logout", text: "LOG-OUT", icon: "mdi-power" },
+    ],
+  }),
+});
