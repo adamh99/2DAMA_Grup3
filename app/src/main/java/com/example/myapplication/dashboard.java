f6a4335f36f99fde16092080adecb6ff2bd6020e
marsanbla <a21marsanbla@inspedralbes.cat>
2022-11-22 09:58:33 +0100

    OptionsMenu

--- app/src/main/java/com/example/myapplication/dashboard.java
+++ app/src/main/java/com/example/myapplication/dashboard.java
@@ -1,10 +1,16 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class dashboard extends AppCompatActivity {
 
@@ -21,4 +27,28 @@ public class dashboard extends AppCompatActivity {
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu,menu);
+
+        return true;
+    }
+
+
+    /*/public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this,"Test", Toast.LENGTH_SHORT);
+
+            case R.id.item2:
+                Toast.makeText(this,"Test", Toast.LENGTH_SHORT);
+
+            case R.id.item3:
+                Toast.makeText(this,"Test", Toast.LENGTH_SHORT);
+
+        }
+        return super.onOptionsItemSelected(item);/*/
+
 }
