9da26f79caf00fdeca3312ae2086246391a88b8f
marsanbla <a21marsanbla@inspedralbes.cat>
2022-11-27 11:33:52 +0100

    Land

--- app/src/main/java/com/example/myapplication/RegisterAct.java
+++ app/src/main/java/com/example/myapplication/RegisterAct.java
@@ -4,23 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterAct extends AppCompatActivity {
     private static final String LOG_TAG =
             RegisterAct.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+
     }
 
-    public void dashboard(View view) {
-        Log.d(LOG_TAG, "Button clicked!");
+
+    public void enter(View view) {
         Intent intent = new Intent(this, dashboard.class);
         startActivity(intent);
     }
-
-
 }
