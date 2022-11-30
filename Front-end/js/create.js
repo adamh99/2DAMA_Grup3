c058eafe140275a975056c9e915bddd573776572
jobofont <a21joaborfon@inspedralbes.cat>
2022-11-30 08:54:07 +0100

    fotos

--- Front-end/js/create.js
+++ Front-end/js/create.js
@@ -1,19 +1,12 @@
 var app = new Vue({
-    el: "#app",
+    el: '#app',
     vuetify: new Vuetify(),
     data: {
         errorMessages: "",
         Nom: "",
         longitud: 0.0,
         latitud: 0.0,
-        state: null,
-        zip: null,
-        country: null,
-        formHasErrors: false,
-
-        user: "",
-        pwd: "",
-        email: ""
+    
     },
 
 
@@ -43,36 +36,6 @@ var app = new Vue({
                     console.log("ERROR!!");
                     console.log(error);
                 });
-        },
-
-
-
-
-
-
-        addressCheck() {
-            this.errorMessages =
-                this.longitud && !this.Nom ? `Hey! I'm required` : "";
-
-            return true;
-        },
-        resetForm() {
-            this.errorMessages = [];
-            this.formHasErrors = false;
-
-            Object.keys(this.form).forEach((f) => {
-                this.$refs[f].reset();
-            });
-        },
-        submit() {
-            this.formHasErrors = false;
-
-            Object.keys(this.form).forEach((f) => {
-                if (!this.form[f]) this.formHasErrors = true;
-
-                this.$refs[f].validate(true);
-            });
-        },
-
-    },
+        }
+    }
 });
